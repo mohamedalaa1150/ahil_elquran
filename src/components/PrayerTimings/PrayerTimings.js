953fa0abdc1715a895462fb1ea3128656643f67c
Mohamed Alaa <77411554+mohamedalaa1150@users.noreply.github.com>
2024-02-29 20:06:13 +0200

    Update PrayerTimings.js

--- src/components/PrayerTimings/PrayerTimings.js
+++ src/components/PrayerTimings/PrayerTimings.js
@@ -5,6 +5,31 @@ import { RxCalendar } from "react-icons/rx";
 import "./PrayerTimings.css";
 import axios from "axios";
 
+// Constant values
+const DAYS_OF_WEEK = [
+  "الأحد",
+  "الاثنين",
+  "الثلاثاء",
+  "الأربعاء",
+  "الخميس",
+  "الجمعة",
+  "السبت",
+];
+const MONTH_NAMES = [
+  "يناير",
+  "فبراير",
+  "مارس",
+  "إبريل",
+  "مايو",
+  "يونيو",
+  "يوليو",
+  "أغسطس",
+  "سبتمبر",
+  "أكتوبر",
+  "نوفمبر",
+  "ديسمبر",
+];
+
 // Date higri
 function getHijriDate(gregorianDate) {
   const options = {
@@ -17,78 +42,19 @@ function getHijriDate(gregorianDate) {
 }
 
 const PrayerTimings = () => {
-  // Day
-  const daysOfWeek = [
-    "الأحد",
-    "الاثنين",
-    "الثلاثاء",
-    "الأربعاء",
-    "الخميس",
-    "الجمعة",
-    "السبت",
-  ];
-  const today = new Date();
-  const dayOfWeek = today.getDay();
-
-  // Time
+  // State variables
   const [time, setTime] = useState("");
+  const [formattedHijriDate, setFormattedHijriDate] = useState("");
+  const [prayerTimes, setPrayerTimes] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  // Effects
   useEffect(() => {
-    const updateClock = () => {
-      const now = new Date();
-      let hours = now.getHours();
-      const minutes = now.getMinutes().toString().padStart(2, "0");
-      const seconds = now.getSeconds().toString().padStart(2, "0");
-      let timePeriod = "ص";
-
-      if (hours >= 12) {
-        timePeriod = "م";
-        if (hours > 12) {
-          hours -= 12;
-        }
-      }
-
-      hours = hours.toString().padStart(2, "0");
-      const timeString =
-        hours + ":" + minutes + ":" + seconds + " " + timePeriod;
-
-      setTime(timeString);
-    };
-
     const intervalId = setInterval(updateClock, 1000);
 
-    return () => {
-      clearInterval(intervalId);
-    };
+    return () => clearInterval(intervalId);
   }, []);
-
-  // Date
-  function getFormattedDate() {
-    const now = new Date();
-    const day = now.getDate().toString().padStart(2, "0");
-    const monthNames = [
-      "يناير",
-      "فبراير",
-      "مارس",
-      "إبريل",
-      "مايو",
-      "يونيو",
-      "يوليو",
-      "أغسطس",
-      "سبتمبر",
-      "أكتوبر",
-      "نوفمبر",
-      "ديسمبر",
-    ];
-    const month = monthNames[now.getMonth()];
-    const year = now.getFullYear();
-
-    return `${day} ${month} ${year}`;
-  }
-  const formattedDate = getFormattedDate();
-
-  // Date Hegry
-  const [formattedHijriDate, setFormattedHijriDate] = useState("");
 
   useEffect(() => {
     const now = new Date();
@@ -96,32 +62,51 @@ const PrayerTimings = () => {
     setFormattedHijriDate(hijriDate);
   }, []);
 
-  // Prayers
-  const [prayerTimes, setPrayerTimes] = useState({});
-  const [city, setCity] = useState("Cairo"); // اسم المدينة
-  const [country, setCountry] = useState("Egypt"); // اسم البلد
-
   useEffect(() => {
     const fetchPrayerTimes = async () => {
+      setIsLoading(true);
       try {
         const response = await axios.get(
-          `http://api.aladhan.com/v1/timingsByCity`,
+          `https://api.aladhan.com/v1/timingsByCity`,
           {
             params: {
-              city: city,
-              country: country,
+              city: "Cairo", // اسم المدينة
+              country: "Egypt", // اسم البلد
               method: 8, // تعيين طريقة حساب المواقيت
             },
           }
         );
         setPrayerTimes(response.data.data.timings);
+        setIsLoading(false);
       } catch (error) {
-        console.error("حدث خطأ في جلب مواقيت الصلاة: ", error);
+        setError("حدث خطأ في جلب مواقيت الصلاة.");
+        setIsLoading(false);
       }
     };
 
     fetchPrayerTimes();
-  }, [city, country]);
+  }, []);
+
+  // Helper functions
+  const updateClock = () => {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    let timePeriod = "ص";
+
+    if (hours >= 12) {
+      timePeriod = "م";
+      if (hours > 12) {
+        hours -= 12;
+      }
+    }
+
+    hours = hours === 0 ? 12 : hours; // تعديل الساعة 0 إلى 12 في نظام 12 ساعة
+
+    const formattedTime = `${hours}:${minutes}:${seconds} ${timePeriod}`;
+    setTime(formattedTime);
+  };
 
   const formatTime = (time) => {
     const parts = time.split(":");
@@ -129,58 +114,61 @@ const PrayerTimings = () => {
     const minute = parts[1];
     return hour > 12 ? `${hour - 12}:${minute} م` : `${hour}:${minute} ص`;
   };
+
+  const getFormattedDate = () => {
+    const now = new Date();
+    const day = now.getDate().toString().padStart(2, "0");
+    const month = MONTH_NAMES[now.getMonth()];
+    const year = now.getFullYear();
+    return `${day} ${month} ${year}`;
+  };
+
   return (
     <div className="prayer-timings">
       <SeactionHead title="مواقيت الصلاة" subtitle="مواقيت الصلاة للمسلمين" />
       <div className="time-section">
         <div className="day">
           <RxCalendar />
-          <p>{daysOfWeek[dayOfWeek]}</p>
+          <p>{DAYS_OF_WEEK[new Date().getDay()]}</p>
         </div>
         <div className="date">
-          <span>{`${formattedDate} م`}</span>
-          <span>{`${formattedHijriDate}`}</span>
+          <span>{`${getFormattedDate()} م`}</span>
+          <span>{formattedHijriDate}</span>
         </div>
         <h2>مواقيت الصلاة في القاهرة - مصر</h2>
         <p>الساعة الآن {time}</p>
-        <div className="prayer">
-          <div className="prayer-card">
-            <h3>الفجر</h3>
-            <span>
-              {formatTime(prayerTimes.Fajr)}
-            </span>
+        {isLoading ? (
+          <p>جارٍ التحميل...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          <div className="prayer">
+            <div className="prayer-card">
+              <h3>الفجر</h3>
+              <span>{formatTime(prayerTimes.Fajr)}</span>
+            </div>
+            <div className="prayer-card">
+              <h3>الشروق</h3>
+              <span>{formatTime(prayerTimes.Sunrise)}</span>
+            </div>
+            <div className="prayer-card">
+              <h3>الظهر</h3>
+              <span>{formatTime(prayerTimes.Dhuhr)}</span>
+            </div>
+            <div className="prayer-card">
+              <h3>العصر</h3>
+              <span>{formatTime(prayerTimes.Asr)}</span>
+            </div>
+            <div className="prayer-card">
+              <h3>المغرب</h3>
+              <span>{formatTime(prayerTimes.Maghrib)}</span>
+            </div>
+            <div className="prayer-card">
+              <h3>العشاء</h3>
+              <span>{formatTime(prayerTimes.Isha)}</span>
+            </div>
           </div>
-          <div className="prayer-card">
-            <h3>الشروق</h3>
-            <span>
-              {formatTime(prayerTimes.Sunrise)}
-            </span>
-          </div>
-          <div className="prayer-card">
-            <h3>الظهر</h3>
-            <span>
-              {formatTime(prayerTimes.Dhuhr)}
-            </span>
-          </div>
-          <div className="prayer-card">
-            <h3>العصر</h3>
-            <span>
-              {formatTime(prayerTimes.Asr)}
-            </span>
-          </div>
-          <div className="prayer-card">
-            <h3>المغرب</h3>
-            <span>
-              {formatTime(prayerTimes.Maghrib)}
-            </span>
-          </div>
-          <div className="prayer-card">
-            <h3>العشاء</h3>
-            <span>
-              {formatTime(prayerTimes.Isha)}
-            </span>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
